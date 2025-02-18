42d65ea107390e89bd4261aae690800d5d5d2caf
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 15:18:24 +0900

    [Bronze V] Title: 초코바, Time: 100 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Bronze/27959. 초코바/초코바.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        if(100 * n >= m) System.out.println("Yes");
+        else System.out.println("No");
+    }
+}

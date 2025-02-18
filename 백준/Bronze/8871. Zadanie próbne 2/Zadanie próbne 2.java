82b6358140a49c2ee407d07103f097119ef0c161
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 17:38:49 +0900

    [Bronze V] Title: Zadanie próbne 2, Time: 192 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Bronze/8871. Zadanie próbne 2/Zadanie próbne 2.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        n += 1;
+        System.out.println(n*2+" "+n*3);
+    }
+}

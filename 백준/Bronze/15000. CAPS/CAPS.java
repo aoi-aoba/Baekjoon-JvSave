f5fee473b1449a7380aeb3a9dbaac5a111dabac4
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 16:58:48 +0900

    [Bronze V] Title: CAPS, Time: 412 ms, Memory: 27964 KB -BaekjoonHub

---
+++ 백준/Bronze/15000. CAPS/CAPS.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        System.out.println(str.toUpperCase());
+    }
+}

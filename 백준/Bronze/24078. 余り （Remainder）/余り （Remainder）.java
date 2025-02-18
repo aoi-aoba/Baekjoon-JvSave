e8656ea6d326011686ef31c065e5c8e06e38df36
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 17:27:56 +0900

    [Bronze V] Title: 余り (Remainder), Time: 172 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/24078. 余り （Remainder）/余り （Remainder）.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        System.out.println(x % 21);
+    }
+}

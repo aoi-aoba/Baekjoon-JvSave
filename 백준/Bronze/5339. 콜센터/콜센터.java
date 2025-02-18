7912a0d3462560bb6c22f2cafe35be6f25b7db8e
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 11:53:16 +0900

    [Bronze V] Title: 콜센터, Time: 96 ms, Memory: 13856 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,18 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        sb.append("     /~\\\n" +
+                "    ( oo|\n" +
+                "    _\\=/_\n" +
+                "   /  _  \\\n" +
+                "  //|/.\\|\\\\\n" +
+                " ||  \\ /  ||\n" +
+                "============\n" +
+                "|          |\n" +
+                "|          |\n" +
+                "|          |");
+        System.out.print(sb);
+    }
+}

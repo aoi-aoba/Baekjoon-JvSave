c8647c3d1b58e91dfeca04b75595eeacce738394
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 11:48:14 +0900

    [Bronze V] Title: 카드 게임, Time: 104 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/5522. 카드 게임/카드 게임.java
@@ -0,0 +1,12 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int res = 0;
+        for(int i=0; i<5; i++)
+            res += Integer.parseInt(br.readLine());
+        System.out.println(res);
+    }
+}

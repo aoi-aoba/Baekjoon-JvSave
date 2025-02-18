a2f389d0757c9df2c23efae3cfc30e2dd3f512b9
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 20:09:36 +0900

    [Bronze V] Title: Correct, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

---
+++ 백준/Bronze/26307. Correct/Correct.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int hour = Integer.parseInt(st.nextToken());
+        int minute = Integer.parseInt(st.nextToken());
+        System.out.println(60*(hour-9)+minute);
+    }
+}

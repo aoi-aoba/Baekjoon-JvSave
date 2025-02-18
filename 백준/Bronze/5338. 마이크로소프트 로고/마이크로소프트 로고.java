a6bb4f1beea9dda6abfb354fe46b1baea5bfe30b
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 11:41:08 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 100 ms, Memory: 13944 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+        System.out.print(sb);
+    }
+}

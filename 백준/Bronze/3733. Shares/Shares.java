954454d9b14a23396b689290d1673bb7bcea013d
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 10:55:06 +0900

    [Bronze V] Title: Shares, Time: 96 ms, Memory: 13964 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str;
+
+        while((str = br.readLine()) != null) {
+            StringTokenizer st = new StringTokenizer(str);
+            int N = Integer.parseInt(st.nextToken());
+            int S = Integer.parseInt(st.nextToken());
+            System.out.println(S/(N+1));
+        }
+    }
+}

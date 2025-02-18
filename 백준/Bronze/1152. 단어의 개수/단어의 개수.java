4abebca8657f3d60b7882f79c7b8a778a645bf69
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 22:45:19 +0900

    [Bronze II] Title: 단어의 개수, Time: 236 ms, Memory: 20072 KB -BaekjoonHub

---
+++ 백준/Bronze/1152. 단어의 개수/단어의 개수.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        StringTokenizer st = new StringTokenizer(str, " ");
+        System.out.print(st.countTokens());
+    }
+}

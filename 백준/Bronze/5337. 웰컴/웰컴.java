7b3af9b7dac36c1650e7f3c86187383e871b2511
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 11:45:29 +0900

    [Bronze V] Title: 웰컴, Time: 96 ms, Memory: 13860 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(".  .   .\n" +
+                "|  | _ | _. _ ._ _  _\n" +
+                "|/\\|(/.|(_.(_)[ | )(/.");
+        System.out.print(sb);
+    }
+}

f255cf9832d2d2786603fa020f60486dec274632
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-18 17:02:34 +0900

    [Bronze V] Title: SciComLove, Time: 172 ms, Memory: 17640 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/21598. SciComLove/SciComLove.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        if(N == 2) System.out.println("SciComLove");
+        System.out.println("SciComLove");
+    }
+}

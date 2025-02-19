cf548bc41c6cb7abeb2ff1a684eb8dde41dfddb3
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-19 21:46:22 +0900

    [Bronze IV] Title: !밀비 급일, Time: 176 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/11365. ！밀비 급일/！밀비 급일.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            String str = sc.nextLine();
+            if(str.equals("END")) break;
+            for(int i=str.length()-1; i>=0; i--)
+                System.out.print(str.charAt(i));
+            System.out.println();
+        }
+    }
+}

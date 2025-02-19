74966ece505c6faa919c97f04ecea45f2be4d6f8
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-19 20:33:31 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 168 ms, Memory: 17232 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        while(true) {
+            if(sc.hasNext()) System.out.println(sc.nextLine());
+            else break;
+        }
+    }
+}

e3d17e19c64bafeb0e054d661a19d33899b3d7dd
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-19 22:11:36 +0900

    [Bronze IV] Title: 특별한 날, Time: 168 ms, Memory: 17676 KB -BaekjoonHub

---
+++ 백준/Bronze/10768. 특별한 날/특별한 날.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        int month = sc.nextInt();
+        int day = sc.nextInt();
+        if(month < 2) System.out.println("Before");
+        else if(month > 2) System.out.println("After");
+        else if(day < 18) System.out.println("Before");
+        else if(day > 18) System.out.println("After");
+        else System.out.println("Special");
+    }
+}

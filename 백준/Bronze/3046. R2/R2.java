a12b15aa512eba97818dddc7ae4acf10eca179af
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-19 15:58:38 +0900

    [Bronze IV] Title: R2, Time: 168 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int R1 = sc.nextInt(), S = sc.nextInt();
+        System.out.println(2*S-R1);
+    }
+}

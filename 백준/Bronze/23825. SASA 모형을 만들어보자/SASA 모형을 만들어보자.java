e7cbda059936655b6f2bdb88e09be3b6f8bc6091
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-20 22:24:40 +0900

    [Bronze IV] Title: SASA 모형을 만들어보자, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/23825. SASA 모형을 만들어보자/SASA 모형을 만들어보자.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int n = scanner.nextInt();
+        int m = scanner.nextInt();
+
+        System.out.println(Math.min(n, m) / 2);
+    }
+}

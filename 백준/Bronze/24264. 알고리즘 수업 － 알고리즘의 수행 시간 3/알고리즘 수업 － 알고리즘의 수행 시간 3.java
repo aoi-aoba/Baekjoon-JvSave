68b0191cdfa30fb655349d9e04a897d0204d437c
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-20 18:18:09 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 3, Time: 176 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/24264. 알고리즘 수업 － 알고리즘의 수행 시간 3/알고리즘 수업 － 알고리즘의 수행 시간 3.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        BigInteger n = sc.nextBigInteger();
+        System.out.println(n.multiply(n));
+        System.out.println(2);
+    }
+}

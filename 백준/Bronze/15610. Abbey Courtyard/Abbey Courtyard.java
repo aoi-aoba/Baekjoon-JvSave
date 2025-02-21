b030f4c2f0831463d3b0e0350b4c071c8f9a2cf3
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-21 14:43:18 +0900

    [Bronze IV] Title: Abbey Courtyard, Time: 172 ms, Memory: 17752 KB -BaekjoonHub

---
+++ 백준/Bronze/15610. Abbey Courtyard/Abbey Courtyard.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        long a = sc.nextLong();
+        System.out.println(4*Math.sqrt(a));
+    }
+}

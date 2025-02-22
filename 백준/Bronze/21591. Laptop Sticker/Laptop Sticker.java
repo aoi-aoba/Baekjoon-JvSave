870224a7e7191c92a485c70277c489a107f247ed
aoi-aoba <aoiaobaprod@gmail.com>
2025-02-22 10:16:47 +0900

    [Bronze IV] Title: Laptop Sticker, Time: 104 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/21591. Laptop Sticker/Laptop Sticker.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int wc = Integer.parseInt(st.nextToken());
+        int hc = Integer.parseInt(st.nextToken());
+        int ws = Integer.parseInt(st.nextToken());
+        int hs = Integer.parseInt(st.nextToken());
+        if(ws <= wc - 2 && hs <= hc - 2) System.out.println(1);
+        else System.out.println(0);
+        br.close();
+    }
+}

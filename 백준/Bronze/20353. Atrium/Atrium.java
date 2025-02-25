80fbe38b7aa5d3af151cfd7bcee3e67837df9fba
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-02-26 08:41:50 +0900

    [Bronze IV] Title: Atrium, Time: 104 ms, Memory: 14220 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long a = Long.parseLong(br.readLine());
+        System.out.println(Math.sqrt(a)*4);
+        br.close();
+    }
+}

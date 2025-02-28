17196e2419b0038d2fe7eb1834fedfdc8d91f8d7
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-02-28 12:14:27 +0900

    [Bronze IV] Title: Cutting Corners, Time: 104 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Bronze/20215. Cutting Corners/Cutting Corners.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int w = Integer.parseInt(st.nextToken());
+        int h = Integer.parseInt(st.nextToken());
+        double diagonal = Math.sqrt(w * w + h * h);
+        System.out.println(w + h - diagonal);
+        br.close();
+    }
+}

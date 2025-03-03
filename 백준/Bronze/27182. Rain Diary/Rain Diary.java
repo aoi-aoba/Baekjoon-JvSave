3a4d02d7415a2c9e204d2873d71c1c6c04ed4478
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-03 16:07:42 +0900

    [Bronze IV] Title: Rain Diary, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

---
+++ 백준/Bronze/27182. Rain Diary/Rain Diary.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int x = Integer.parseInt(st.nextToken());
+        int y = Integer.parseInt(st.nextToken());
+        if(x > 7) System.out.println(x-7);
+        else System.out.println(y+7);
+        br.close();
+    }
+}

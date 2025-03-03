c6cef1efc31b95126a917edabfaa1bcf15ac0981
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-03 20:57:15 +0900

    [Bronze IV] Title: НАМИСЛИХ СИ ЧИСЛО, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/31261. НАМИСЛИХ СИ ЧИСЛО/НАМИСЛИХ СИ ЧИСЛО.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int mid = (a + b) * a;
+        int fin = (mid + a) * a;
+        System.out.println(fin);
+    }
+}

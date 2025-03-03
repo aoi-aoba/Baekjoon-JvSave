3cf12e46f0c360fe8385b2747c7ad72fb1c57142
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-03 21:55:14 +0900

    [Bronze IV] Title: Alea Iacta Est, Time: 100 ms, Memory: 14180 KB -BaekjoonHub

---
+++ 백준/Bronze/33046. Alea Iacta Est/Alea Iacta Est.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+        int C = Integer.parseInt(st.nextToken());
+        int D = Integer.parseInt(st.nextToken());
+        int answer = (A + B - 1) % 4;
+        answer = (answer + C + D - 1) % 4;
+        System.out.println(answer + 1);
+        br.close();
+    }
+}

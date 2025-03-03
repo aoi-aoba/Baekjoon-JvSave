68e388b266161758b8bea89cf1b1b7d7820212b8
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-03 14:11:53 +0900

    [Bronze IV] Title: 가희와 클럽 오디션 1, Time: 108 ms, Memory: 14164 KB -BaekjoonHub

---
+++ 백준/Bronze/30794. 가희와 클럽 오디션 1/가희와 클럽 오디션 1.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br= new BufferedReader( new InputStreamReader(System.in));
+        StringTokenizer st =new StringTokenizer(br.readLine());
+        int lv = Integer.parseInt(st.nextToken());
+        String judgement = st.nextToken();
+
+        if(judgement.equals("miss")) {
+            lv *= 0;
+        } else if(judgement.equals("bad")) {
+            lv *= 200;
+        } else if(judgement.equals("cool")) {
+            lv *= 400;
+        } else if(judgement.equals("great")) {
+            lv *= 600;
+        } else {
+            lv *= 1000;
+        }
+
+        System.out.println(lv);
+    }
+}

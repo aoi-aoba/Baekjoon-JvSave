34a8260ccf51ef17f92aa192f1195b74b8cc1c7a
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-03 17:42:25 +0900

    [Bronze IV] Title: Cornhole, Time: 120 ms, Memory: 15868 KB -BaekjoonHub

---
+++ 백준/Bronze/27855. Cornhole/Cornhole.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h1 = Integer.parseInt(st.nextToken());
+        int b1 = Integer.parseInt(st.nextToken());
+
+        st = new StringTokenizer(br.readLine());
+        int h2 = Integer.parseInt(st.nextToken());
+        int b2 = Integer.parseInt(st.nextToken());
+
+        int p1Score = 3*h1+b1;
+        int p2Score = 3*h2+b2;
+
+        String s = "NO SCORE";
+        if(p1Score > p2Score) s = "1 " + (p1Score-p2Score);
+        else if(p2Score > p1Score) s = "2 " + (p2Score-p1Score);
+        System.out.println(s);
+
+        br.close();
+    }
+}

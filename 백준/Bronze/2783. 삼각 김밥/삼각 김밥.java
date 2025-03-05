577b7d1fefef7ea02085f4d0089fa099b23519d1
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-05 09:22:22 +0900

    [Bronze III] Title: 삼각 김밥, Time: 112 ms, Memory: 14516 KB -BaekjoonHub

---
+++ 백준/Bronze/2783. 삼각 김밥/삼각 김밥.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        double X = Double.parseDouble(st.nextToken());
+        double Y = Double.parseDouble(st.nextToken());
+        double R = (1000 / Y) * X;
+
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            double x = Double.parseDouble(st.nextToken());
+            double y = Double.parseDouble(st.nextToken());
+            double r = (1000 / y) * x;
+
+            R = Math.min(R, r);
+        }
+
+        System.out.printf("%.2f%n", R);
+        br.close();
+    }
+}

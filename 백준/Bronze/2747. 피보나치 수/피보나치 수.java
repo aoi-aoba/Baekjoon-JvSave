6ed20c41bc2a2def2bd2f11fc34e27da748196ab
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-07 18:03:53 +0900

    [Bronze II] Title: 피보나치 수, Time: 108 ms, Memory: 14176 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static long fib_fast(int n) {
+        if(n == 0) return 0;
+        else if(n == 1 || n == 2) return 1;
+
+        long[] fib = new long[n+1];
+        fib[0] = 0; fib[1] = 1; fib[2] = 1;
+
+        for(int i = 3; i <= n; i++) {
+            fib[i] = fib[i-1] + fib[i-2];
+        }
+
+        return fib[n];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        System.out.println(fib_fast(n));
+        br.close();
+    }
+}

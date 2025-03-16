087fc3c626d6962ea6a74e8c6e261bbc1fb29473
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-16 15:40:19 +0900

    [Bronze III] Title: 더하기, Time: 104 ms, Memory: 13976 KB -BaekjoonHub

---
+++ 백준/Bronze/9085. 더하기/더하기.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        for(int i=0; i<T; i++) {
+            int N = Integer.parseInt(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int sum = 0;
+            for(int j=0; j<N; j++) sum += Integer.parseInt(st.nextToken());
+            System.out.println(sum);
+        }
+        br.close();
+    }
+}

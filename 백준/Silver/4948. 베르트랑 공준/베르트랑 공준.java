3695cb1097d931484195e06f3ff317d0efaa0688
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-23 15:50:29 +0900

    [Silver II] Title: 베르트랑 공준, Time: 228 ms, Memory: 61324 KB -BaekjoonHub

---
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static int[] primeSieve(int n) {
+        int[] arr = new int[2 * n + 1];
+        for (int i = 2; i <= 2 * n; i++) arr[i] = i;
+        for (int i = 2; i <= 2 * n; i++) {
+            if (arr[i] == 0) continue;
+            for (int j = 2 * i; j <= 2 * n; j += i) arr[j] = 0;
+        }
+        return arr;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        while (true) {
+            int n = Integer.parseInt(br.readLine()), cnt = 0;
+            if(n == 0) break;
+            int[] prime = primeSieve(n);
+            for (int i = n+1; i <= 2 * n; i++)
+                if (prime[i] != 0) cnt++;
+            sb.append(cnt).append("\n");
+        }
+        System.out.print(sb);
+        br.close();
+    }
+}

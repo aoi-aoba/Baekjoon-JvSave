18a8788d1979e5bb8ec9556d868928f1f36155c0
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-23 12:41:01 +0900

    [Silver III] Title: 1로 만들기, Time: 164 ms, Memory: 18664 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -8,7 +8,7 @@ public class Main {
         int[] arr = new int[1000001];
         Arrays.fill(arr, 0);
         arr[1] = 0;
-        for(int i=1; i<1000000; i++) {
+        for(int i=1; i<N; i++) {
             if(arr[i+1] > arr[i] + 1 || arr[i+1] == 0) arr[i+1] = arr[i] + 1;
             if(i * 2 < 1000001) if(arr[i*2] > arr[i] + 1 || arr[i*2] == 0) arr[i*2] = arr[i] + 1;
             if(i * 3 < 1000001) if(arr[i*3] > arr[i] + 1 || arr[i*3] == 0) arr[i*3] = arr[i] + 1;

ff20d2823092c929f7d681c992d3606dbde97c55
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-25 16:12:01 +0900

    [Silver II] Title: 키로거, Time: 884 ms, Memory: 280128 KB -BaekjoonHub

--- 백준/Silver/5397. 키로거/키로거.java
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -4,26 +4,24 @@ import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder result = new StringBuilder();
-        int T = Integer.parseInt(br.readLine());
-        for(int t = 0; t < T; t++) {
-            StringBuilder sb = new StringBuilder();
-            String str = br.readLine();
-            Stack<String> res = new Stack<>(), left = new Stack<>();
-            for(int i = 0; i < str.length(); i++) {
-                char ch = str.charAt(i);
-                switch(ch) {
-                    case '<' : if(!res.isEmpty()) left.push(res.pop()); break;
-                    case '>' : if(!left.isEmpty()) res.push(left.pop()); break;
-                    case '-' : if(!res.isEmpty()) res.pop();break;
-                    default: res.push(String.valueOf(ch)); break;
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for(int i=0; i<N; i++) {
+            char[] arr = br.readLine().toCharArray();
+            LinkedList<String> linkedList = new LinkedList<>();
+            ListIterator<String> iterator = linkedList.listIterator();
+            for (char ch : arr) {
+                switch (ch) {
+                    case '<': if (iterator.hasPrevious()) iterator.previous(); break;
+                    case '>': if (iterator.hasNext()) iterator.next(); break;
+                    case '-': if (iterator.hasPrevious()) { iterator.previous(); iterator.remove(); } break;
+                    default: iterator.add(String.valueOf(ch));
                 }
             }
-            while(!left.isEmpty()) res.push(left.pop());
-            while(!res.isEmpty()) sb.append(res.pop());
-            result.append(sb.reverse()).append("\n");
+            iterator = linkedList.listIterator();
+            while (iterator.hasNext()) sb.append(iterator.next());
+            sb.append("\n");
         }
-        System.out.print(result);
-        br.close();
+        System.out.print(sb);
     }
 }

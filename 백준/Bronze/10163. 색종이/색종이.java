c0c2e183b8cb4e46651dee488fef8bc2a5b7e1b6
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-03-28 16:44:39 +0900

    [Bronze I] Title: 색종이, Time: 424 ms, Memory: 23920 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.java
+++ 백준/Bronze/10163. 색종이/색종이.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[][] map = new int[101][101];
+        int[][] map = new int[1001][1001];
         for(int tc = 1; tc <= N; tc++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken()), b = Integer.parseInt(st.nextToken());
@@ -16,8 +16,8 @@ public class Main {
         }
         for(int rep = 1; rep <= N; rep++) {
             int count = 0;
-            for(int i = 0; i < 101; i++)
-                for(int j = 0; j < 101; j++)
+            for(int i = 0; i < 1001; i++)
+                for(int j = 0; j < 1001; j++)
                     if(map[i][j] == rep) count++;
             System.out.println(count);
         }

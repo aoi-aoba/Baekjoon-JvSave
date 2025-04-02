b5bec938b19125f01711e75741a67aae5e34f4e7
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-02 14:52:07 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 640 ms, Memory: 125440 KB -BaekjoonHub

---
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken()), K = Integer.parseInt(st.nextToken()), cnt = 0;
+        Stack<Integer>[] stacks = new Stack[N];
+        Arrays.fill(stacks, null);
+        for(int i=0; i<N; i++) {
+            st = new StringTokenizer(br.readLine());
+            int line = Integer.parseInt(st.nextToken()) - 1, fret = Integer.parseInt(st.nextToken());
+            if(stacks[line] == null) {
+                Stack<Integer> stack = new Stack<>();
+                stack.push(fret);
+                stacks[line] = stack;
+                cnt++;
+            } else if(stacks[line].get(0) > fret) {
+                cnt += stacks[line].size() + 1;
+                stacks[line].clear();
+                stacks[line].push(fret);
+            } else {
+                while(stacks[line].peek() > fret) {
+                    stacks[line].pop();
+                    cnt++;
+                }
+                if(stacks[line].peek() < fret) {
+                    stacks[line].push(fret);
+                    cnt++;
+                }
+            }
+        }
+        System.out.println(cnt);
+        br.close();
+    }
+}

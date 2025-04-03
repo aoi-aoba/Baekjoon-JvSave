abcd5e173a3ceb6c112414d33c9988f3b7079159
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-03 17:29:30 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 16044 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static int[][] Fibonacci = new int[41][41];
+    public static int[][] Fibonacci = new int[41][2];
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 

c470b75fa6282d2c414daf4d2c79cab68d533e00
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-03 20:09:46 +0900

    [Silver V] Title: 다리 놓기, Time: 108 ms, Memory: 14292 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.java
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -1,30 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static BigInteger[] Factorial = new BigInteger[31];
-    public static BigInteger Combination(int n, int r) {
-        BigInteger molecular = Factorial[n];
-        BigInteger denominator = Factorial[r].multiply(Factorial[n-r]);
-        return molecular.divide(denominator);
-    }
-    public static void SetArrayFactorial() {
-        Factorial[0] = BigInteger.ONE;
-        Factorial[1] = BigInteger.ONE;
-        for(int i = 2; i < 31; i++) Factorial[i] = Factorial[i-1].multiply(BigInteger.valueOf(i));
+    public static int[][] Combination = new int[31][31];
+    public static void setCombination() {
+        Combination[1][0] = 1;
+        Combination[1][1] = 1;
+        for(int i = 2; i < 31; i++) for(int j = 0; j <= i; j++) {
+            if (j == 0 || j == i) Combination[i][j] = 1;
+            else Combination[i][j] = Combination[i - 1][j - 1] + Combination[i - 1][j];
+        }
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+        setCombination();
         int T = Integer.parseInt(br.readLine());
-        SetArrayFactorial();
         while(T-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int R = Integer.parseInt(st.nextToken()), N = Integer.parseInt(st.nextToken());
-            sb.append(Combination(N, R)).append("\n");
+            sb.append(Combination[N][R]).append("\n");
         }
         System.out.print(sb);
         br.close();

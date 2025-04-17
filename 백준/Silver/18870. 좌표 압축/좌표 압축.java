72868e3591e9552d6a892bb509c35cc26de728ac
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-18 06:59:24 +0900

    [Silver II] Title: 좌표 압축, Time: 1816 ms, Memory: 263336 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -2,40 +2,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][2];
+
+        int N = Integer.parseInt(br.readLine()), depressedRank = 0;
+        int[] originArr = new int[N], sortedArr = new int[N];
+        Map<Integer, Integer> hashMap = new HashMap<>();
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++)
-            arr[i] = new int[] {Integer.parseInt(st.nextToken()), i};
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[0], o2[0]);
-            }
-        });
-        int prev = Integer.MIN_VALUE, changeVal = -1;
-        for (int[] temp : arr) {
-            if (temp[0] != prev) {
-                prev = temp[0];
-                temp[0] = ++changeVal;
-            } else temp[0] = changeVal;
-        }
-        Arrays.sort(arr, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] o1, int[] o2) {
-                return Integer.compare(o1[1], o2[1]);
-            }
-        });
-        for (int[] temp : arr) {
-            sb.append(temp[0]).append(" ");
-        }
+            originArr[i] = sortedArr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(sortedArr);
+
+        for (int i = 0; i < N; i++)
+            if (!hashMap.containsKey(sortedArr[i]))
+                hashMap.put(sortedArr[i], depressedRank++);
+
+        for (int num : originArr)
+            sb.append(hashMap.get(num)).append(" ");
+
         System.out.println(sb);
     }
 }

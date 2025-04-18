5f585970ccf958b5fbacfd0a510e5a5986490f4b
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-18 22:27:12 +0900

    [Silver IV] Title: 문자열 집합, Time: 344 ms, Memory: 30900 KB -BaekjoonHub

--- 백준/Silver/14425. 문자열 집합/문자열 집합.java
+++ 백준/Silver/14425. 문자열 집합/문자열 집합.java
@@ -1,21 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    public static int binarySearch(String[] stringArray, String target) {
+        int begin = 0, end = stringArray.length - 1;
+        while (begin <= end) {
+            int mid = (begin + end) / 2;
+            if (target.equals(stringArray[mid])) return mid;
+            else if (target.compareTo(stringArray[mid]) < 0) end = mid - 1;
+            else begin = mid + 1;
+        }
+        return -1;
+    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken()), M = Integer.parseInt(st.nextToken()), cnt = 0;
-        Set<String> hashSet = new HashSet<>();
-        while (N-- > 0) hashSet.add(br.readLine());
-        while (M-- > 0) {
-            String str = br.readLine();
-            if (hashSet.contains(str)) cnt++;
-        }
+        String[] input = new String[N];
+        for (int i = 0; i < N; i++) input[i] = br.readLine();
+        Arrays.sort(input);
+        for (int i = 0; i < M; i++)
+            if (binarySearch(input, br.readLine()) >= 0) cnt++;
         System.out.println(cnt);
         br.close();
     }

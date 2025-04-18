7cb9aabe82e6e21a15514444a0ee6ee4303e6ed2
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-18 16:41:55 +0900

    [Silver III] Title: 서로 다른 부분 문자열의 개수, Time: 340 ms, Memory: 199980 KB -BaekjoonHub

--- 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
+++ 백준/Silver/11478. 서로 다른 부분 문자열의 개수/서로 다른 부분 문자열의 개수.java
@@ -1,20 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        Set<String> hashSet = new HashSet<>();
-        for (int i = 0; i <= str.length(); i++) {
-            for (int j = i+1; j <= str.length(); j++) {
-                hashSet.add(str.substring(i, j));
+        String str = br.readLine(), suffix = "", subStr = "";
+        int cnt = 0;
+
+        for (int i = str.length() - 1; i >= 0; i--) { // 우측부터 문자열 탐색
+            suffix = str.substring(i+1); // 현재 i보다 뒤에 있는 문자열을 잘라내기
+            for (int j = i; j < str.length(); j++) {
+                subStr = str.substring(i, j+1); // i부터 j+1까지 substring을 생성
+                if (!suffix.contains(subStr)) cnt++; // suffix가 subStr을 포함하지 않으면 cnt 증가
             }
         }
-        System.out.println(hashSet.size());
+        // 문자열을 뒤에서부터 읽는 suffix가 있기 때문에 subStr은 이미 suffix 선에서 중복 처리가 가능함
+        // 이미 나왔던 subStr은 전부 suffix가 'contain'하고 있음
+
+        System.out.println(cnt);
         br.close();
     }
 }

3fc67ba68125f92fb13113a9c5d8465af5ed2749
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-18 15:42:50 +0900

    [Silver II] Title: 통계학, Time: 688 ms, Memory: 49708 KB -BaekjoonHub

---
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static int avg(int[] arr) {
+        int sum = 0;
+        for (int num : arr) sum += num;
+        double avgInDouble = (double)sum / arr.length;
+        return (avgInDouble < 0) ? (int)(avgInDouble-0.5) : (int)(avgInDouble+0.5);
+    }
+    public static int mid(int[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length/2];
+    }
+    public static int freq(int[] arr) {
+        int freq = 0;
+        List<Integer> list = new ArrayList<>();
+        int[] cnt = new int[8001];
+        for (int num : arr) {
+            cnt[num+4000]++;
+            if (cnt[num+4000] > freq) freq = cnt[num+4000];
+        }
+        for (int i = 0; i < 8001; i++) if (cnt[i] == freq) list.add(i-4000);
+        Collections.sort(list);
+        if (list.size() >= 2) return list.get(1);
+        return list.get(0);
+    }
+    public static int range(int[] arr) {
+        Arrays.sort(arr);
+        return arr[arr.length-1] - arr[0];
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) arr[i] = Integer.parseInt(br.readLine());
+        Arrays.sort(arr);
+
+        System.out.println(avg(arr));
+        System.out.println(mid(arr));
+        System.out.println(freq(arr));
+        System.out.println(range(arr));
+        br.close();
+    }
+}

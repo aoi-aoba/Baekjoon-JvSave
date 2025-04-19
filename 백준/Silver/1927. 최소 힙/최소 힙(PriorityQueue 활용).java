dea048f7987d8ea480e957216b2da4a483bd89e8
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-19 22:53:25 +0900

    Create 최소 힙(PriorityQueue 활용).java

---
+++ 백준/Silver/1927. 최소 힙/최소 힙(PriorityQueue 활용).java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Queue<Integer> heap = new PriorityQueue<>();
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            int temp = Integer.parseInt(br.readLine()), peak = 0;
+            if (temp == 0 && !heap.isEmpty()) peak = heap.poll();
+            else if (temp != 0) heap.add(temp);
+            if (temp == 0) sb.append(peak).append("\n");
+        }
+        System.out.print(sb);
+    }
+}

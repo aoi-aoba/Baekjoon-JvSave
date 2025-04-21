469505f3c701ef78e22baffa60228f3d770ac2e4
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-21 21:22:42 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -4,26 +4,25 @@ import java.io.InputStreamReader;
 
 public class Main {
     public static int[] stair, dp;
-    public static void goUpstairsDP(int N) {
-        dp = new int[N];
-        dp[0] = stair[0];
-        if (N == 1) return;
-        dp[1] = stair[0] + stair[1];
-        if (N == 2) return;
-        dp[2] = Math.max(stair[0]+stair[2], stair[1]+stair[2]);
+    public static int goUpstairsDP(int N) {
+        if (N == 1) return stair[0];
+        if (N == 2) return stair[0] + stair[1];
+        if (N == 3) return Math.max(stair[0]+stair[2], stair[1]+stair[2]);
+        int dp0 = stair[0], dp1 = stair[0] + stair[1], dp2 = Math.max(stair[0]+stair[2], stair[1]+stair[2]);
         for (int i = 3; i < N; i++) {
-            int twoStairs = dp[i-2] + stair[i];
-            int oneStairs = dp[i-3] + stair[i-1] + stair[i];
-            dp[i] = Math.max(twoStairs, oneStairs);
+            int dpNew = Math.max(dp1 + stair[i], dp0 + stair[i-1] + stair[i]);
+            dp0 = dp1;
+            dp1 = dp2;
+            dp2 = dpNew;
         }
+        return dp2;
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         stair = new int[N];
         for (int i = 0; i < N; i++) stair[i] = Integer.parseInt(br.readLine());
-        goUpstairsDP(N);
-        System.out.println(dp[N-1]);
+        System.out.println(goUpstairsDP(N));
         br.close();
     }
 }

af3bc6262c1c21afe9d0769f450291b0c3acb4ab
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-23 23:24:42 +0900

    [Silver V] Title: 거스름돈, Time: 104 ms, Memory: 14224 KB -BaekjoonHub

--- 백준/Silver/14916. 거스름돈/거스름돈.java
+++ 백준/Silver/14916. 거스름돈/거스름돈.java
@@ -3,26 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-    public static int[] dp = new int[100001];
-    public static void dp() {
-        dp[0] = dp[1] = dp[3] = -1;
-        dp[2] = dp[5] = 1;
-        dp[4] = 2;
-        for (int i = 6; i < 100001; i++) {
-            int tempCase1 = -1, tempCase2 = -1;
-            if (dp[i-2] != -1) tempCase1 = dp[i-2] + 1;
-            if (dp[i-5] != -1) tempCase2 = dp[i-5] + 1;
-            if (tempCase1 == -1 && tempCase2 == -1) dp[i] = -1;
-            else if (tempCase1 == -1) dp[i] = tempCase2;
-            else if (tempCase2 == -1) dp[i] = tempCase1;
-            else dp[i] = Math.min(tempCase1, tempCase2);
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        dp();
-        System.out.println(dp[n]);
+        int n = Integer.parseInt(br.readLine()), maxFive = n / 5, res = -1;
+        if (maxFive == 0) {
+            if (n % 2 == 0) res = n / 2;
+        } else {
+            int[] arr = new int[maxFive + 1];
+            for (int i = maxFive; i >= 0; i--) {
+                if((n - 5 * i) % 2 == 0) arr[i] = i + (n - 5 * i) / 2;
+                else arr[i] = -1;
+            }
+            int min = 100000;
+            for (int i : arr)
+                if (i != -1) min = Math.min(i, min);
+            res = min;
+        }
+        System.out.println(res);
         br.close();
     }
 }

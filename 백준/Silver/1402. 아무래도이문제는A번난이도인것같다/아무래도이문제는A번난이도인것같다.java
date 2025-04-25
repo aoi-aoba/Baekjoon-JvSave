4a7e9a94dc45ceccf5f3deba200d382819ba2122
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-25 22:01:49 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 96 ms, Memory: 14084 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            br.readLine();
+            System.out.println("yes");
+        }
+        br.close();
+    }
+}

f3db2bfde4609620ae56fcfa9a2d2bdb365af950
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-26 12:03:26 +0900

    [Silver IV] Title: 대칭 차집합, Time: 880 ms, Memory: 81032 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -11,14 +11,15 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a_n = Integer.parseInt(st.nextToken()), b_n = Integer.parseInt(st.nextToken());
 
-        Set<Integer> setA = new TreeSet<>(), setB = new TreeSet<>(), res = new TreeSet<>();
-        StringTokenizer stForSetA = new StringTokenizer(br.readLine());
-        while (stForSetA.hasMoreTokens()) setA.add(Integer.parseInt(stForSetA.nextToken()));
-        StringTokenizer stForSetB = new StringTokenizer(br.readLine());
-        while (stForSetB.hasMoreTokens()) setB.add(Integer.parseInt(stForSetB.nextToken()));
-
-        for (int i : setA) if (!setB.contains(i)) res.add(i);
-        for (int j : setB) if (!setA.contains(j)) res.add(j);
+        Set<Integer> res = new TreeSet<>();
+        StringTokenizer stSetA = new StringTokenizer(br.readLine());
+        while (stSetA.hasMoreTokens()) res.add(Integer.parseInt(stSetA.nextToken()));
+        StringTokenizer stSetB = new StringTokenizer(br.readLine());
+        while (stSetB.hasMoreTokens()) {
+            int temp = Integer.parseInt(stSetB.nextToken());
+            if (res.contains(temp)) res.remove(temp);
+            else res.add(temp);
+        }
 
         System.out.println(res.size());
         br.close();

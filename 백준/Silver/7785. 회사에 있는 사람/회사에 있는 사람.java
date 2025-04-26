52f18ec3eaa1a3f82c950cf364d1feb420665a10
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-26 10:54:59 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 464 ms, Memory: 48024 KB -BaekjoonHub

--- 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.java
@@ -6,15 +6,20 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
         int N = Integer.parseInt(br.readLine());
         Set<String> treeSet = new TreeSet<>(Comparator.reverseOrder());
+
         while (N-- > 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             String name = st.nextToken(), status = st.nextToken();
             if (!treeSet.contains(name) && status.charAt(0) == 'e') treeSet.add(name);
             else if (status.charAt(0) == 'l') treeSet.remove(name);
         }
-        for (String str : treeSet) System.out.println(str);
+
+        for (String str : treeSet) sb.append(str).append("\n");
+        System.out.print(sb);
         br.close();
     }
 }

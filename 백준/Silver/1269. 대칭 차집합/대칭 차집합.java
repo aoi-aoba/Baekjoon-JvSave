749af9820b285d796bd476c693cb9c3c0acc6c5d
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-26 12:06:34 +0900

    [Silver IV] Title: 대칭 차집합, Time: 608 ms, Memory: 101016 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.java
@@ -1,9 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
-import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -11,7 +11,7 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int a_n = Integer.parseInt(st.nextToken()), b_n = Integer.parseInt(st.nextToken());
 
-        Set<Integer> res = new TreeSet<>();
+        Set<Integer> res = new HashSet<>();
         StringTokenizer stSetA = new StringTokenizer(br.readLine());
         while (stSetA.hasMoreTokens()) res.add(Integer.parseInt(stSetA.nextToken()));
         StringTokenizer stSetB = new StringTokenizer(br.readLine());

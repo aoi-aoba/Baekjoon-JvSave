e150c214be6ae488add05a76ab01cd037cecc401
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-04-30 20:26:33 +0900

    [Silver V] Title: 덩치, Time: 100 ms, Memory: 14168 KB -BaekjoonHub

---
+++ 백준/Silver/7568. 덩치/덩치.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N+1][2];
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        for (int i = 1; i <= N; i++) {
+            int rank = 1;
+            for (int j = 1; j <= N; j++) {
+                if (i == j) continue;
+                if (arr[i][0] < arr[j][0] && arr[i][1] < arr[j][1]) rank++;
+            }
+            sb.append(rank).append(" ");
+        }
+
+        System.out.println(sb);
+        br.close();
+    }
+}

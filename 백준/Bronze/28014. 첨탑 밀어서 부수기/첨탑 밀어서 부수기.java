e801c51af0f7293cf9dd76f038c9376859666a51
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-05-02 15:42:47 +0900

    [Bronze III] Title: 첨탑 밀어서 부수기, Time: 820 ms, Memory: 330044 KB -BaekjoonHub

---
+++ 백준/Bronze/28014. 첨탑 밀어서 부수기/첨탑 밀어서 부수기.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine()), now = 0, cnt = 0;
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int temp = Integer.parseInt(st.nextToken());
+            if (now <= temp) cnt++;
+            now = temp;
+        }
+        System.out.println(cnt);
+        br.close();
+    }
+}

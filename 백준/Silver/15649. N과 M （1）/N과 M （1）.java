0b5f641f2303280e446003b2be8a76743b657903
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-05-23 09:54:59 +0900

    [Silver III] Title: N과 M (1), Time: 284 ms, Memory: 26300 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static int N, M;
+    public static StringBuilder result = new StringBuilder();
+    public static boolean[] visited;
+    public static void find(ArrayList<Integer> list, int depth) {
+        if (depth == M) {
+            for (int temp : list) {
+                result.append(temp).append(" ");
+            } result.append("\n");
+            return;
+        }
+        for (int i = 1; i <= N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                list.add(i);
+                find(list, depth + 1);
+                list.remove(list.size()-1);
+                visited[i] = false;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        for (int i = 1; i <= N; i++) {
+            visited = new boolean[N+1];
+            visited[i] = true;
+            ArrayList<Integer> list = new ArrayList<>();
+            list.add(i);
+            find(list, 1);
+        }
+        System.out.println(result);
+    }
+}

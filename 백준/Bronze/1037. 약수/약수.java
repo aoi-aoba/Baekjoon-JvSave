745389543aa7e872aff577e580fd361603842c28
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-05-27 08:49:26 +0900

    [Bronze I] Title: 약수, Time: 64 ms, Memory: 11580 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.java
+++ 백준/Bronze/1037. 약수/약수.java
@@ -8,24 +8,14 @@ public class Main {
         StringTokenizer st;
 
         int realDivisorNum = Integer.parseInt(br.readLine());
-        int[] realDivisorArr = new int[realDivisorNum];
         st = new StringTokenizer(br.readLine());
+        int min = Integer.MAX_VALUE, max = Integer.MIN_VALUE;
         for(int i=0; i<realDivisorNum; i++) {
-            realDivisorArr[i] = Integer.parseInt(st.nextToken());
+            int temp = Integer.parseInt(st.nextToken());
+            min = Math.min(temp, min);
+            max = Math.max(temp, max);
         }
-        if(realDivisorNum == 1) {
-            System.out.println(realDivisorArr[0] * realDivisorArr[0]);
-        } else {
-            Arrays.sort(realDivisorArr); // 임의로 주어질 경우 최대최소를 구해야 하므로
-            System.out.println(realDivisorArr[0] * realDivisorArr[realDivisorNum - 1]);
-        }
+        System.out.println(min*max);
         br.close();
     }
 }
-
-/*
-N의 약수에 대하여 A가 1과 N이 아닌 모든 약수를 진짜 약수라고 한다면
-진짜 약수의 최소와 최대의 곱은 N이다 (1이 아닌 최소의 약수가 나올 것이므로 N/최소약수 = 최대약수)
-1과 자기 자신만을 가지는 소수는 나올수 없고, 소수의 제곱수들은 한 개만을 인수로 가질텐데
-이 인수를 그냥 두 번 곱해주면 N이 나올 것이다 (가령, 9는 3만 나올 것이므로 3^2=9)
- */

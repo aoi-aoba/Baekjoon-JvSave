bf545995df555f676f663c4f2d1475252be21cf9
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-05-27 13:58:57 +0900

    [Gold V] Title: 암호 만들기, Time: 156 ms, Memory: 18828 KB -BaekjoonHub

--- 백준/Gold/1759. 암호 만들기/암호 만들기.java
+++ 백준/Gold/1759. 암호 만들기/암호 만들기.java
@@ -3,14 +3,13 @@ import java.util.*;
 
 public class Main {
     public static char[] chars;
-    public static boolean[] visited;
     public static int L, C;
     public static StringBuilder result = new StringBuilder();
+    public static final Set<Character> vowelSet = Set.of('a', 'e', 'i', 'o', 'u');
     public static void checkPrinting(String target) {
-        char[] targetArr = target.toCharArray();
         int vowels = 0, consonants = 0;
-        for (char ch : targetArr) {
-            if (ch == 'a' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'e') vowels++;
+        for (char ch : target.toCharArray()) {
+            if (vowelSet.contains(ch)) vowels++;
             else consonants++;
         }
         if (vowels >= 1 && consonants >= 2) result.append(target).append("\n");
@@ -20,11 +19,7 @@ public class Main {
             checkPrinting(nowString);
             return;
         } else for (int i = prev; i < C; i++) {
-            if (!visited[i]) {
-                visited[i] = true;
-                tracker(depth+1, i,nowString+chars[i]);
-                visited[i] = false;
-            }
+            tracker(depth+1, i+1,nowString+chars[i]);
         }
     }
     public static void main(String[] args) throws IOException {
@@ -34,7 +29,6 @@ public class Main {
         C = Integer.parseInt(st.nextToken());
 
         chars = new char[C];
-        visited = new boolean[C];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < C; i++) chars[i] = st.nextToken().charAt(0);
         Arrays.sort(chars);

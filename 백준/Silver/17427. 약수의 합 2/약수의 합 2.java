285ef77af5462cf6ebb1bfccb5a5f79cd2574cc6
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-05-27 09:35:24 +0900

    [Silver II] Title: 약수의 합 2, Time: 136 ms, Memory: 19636 KB -BaekjoonHub

--- 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
+++ 백준/Silver/17427. 약수의 합 2/약수의 합 2.java
@@ -1,13 +1,26 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        final int MAX = 1_000_000;
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        long n = Long.parseLong(br.readLine()), result = 0;
-        for(int i=1; i<=n; i++)
-            result += i * (n / i);
-        System.out.println(result);
-        br.close();
+        int N = Integer.parseInt(br.readLine());
+
+        long[] f = new long[N + 1];  // f[i] = i의 약수의 합
+
+        // 약수의 합 구하기: 에라토스테네스의 체처럼 처리
+        for (int i = 1; i <= N; i++) {
+            for (int j = i; j <= N; j += i) {
+                f[j] += i;
+            }
+        }
+
+        // g(N) = f(1) + f(2) + ... + f(N)
+        long g = 0;
+        for (int i = 1; i <= N; i++) {
+            g += f[i];
+        }
+
+        System.out.println(g);
     }
 }

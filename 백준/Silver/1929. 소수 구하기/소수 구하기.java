c0df9ca904c29031ee754a553fa47a4cc69d25a9
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-06-03 23:21:07 +0900

    [Silver III] Title: 소수 구하기, Time: 160 ms, Memory: 21712 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.java
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -1,13 +1,13 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static int[] primeSieve(int n) {
-        int[] arr = new int[n+1];
-        for(int i=2; i <= n; i++) arr[i] = i;
-        for(int i=2; i<=n; i++) {
-            if(arr[i] == 0) continue;
-            for(int j=2*i; j<=n; j+=i) arr[j] = 0;
+        int[] arr = new int[n + 1];
+        for (int i = 2; i <= n; i++) arr[i] = i;
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (arr[i] == 0) continue;
+            for (int j = 2 * i; j <= n; j += i) arr[j] = 0;
         }
         return arr;
     }
@@ -17,7 +17,7 @@ public class Main {
         StringBuilder sb = new StringBuilder();
         int m = Integer.parseInt(st.nextToken()), n = Integer.parseInt(st.nextToken());
         int[] prime = primeSieve(n);
-        for(int i=m; i<=n; i++) if(prime[i] != 0) sb.append(prime[i]).append("\n");
+        for (int i = m; i <= n; i++) if (prime[i] != 0) sb.append(prime[i]).append("\n");
         System.out.print(sb);
         br.close();
     }

965d00057f58fd513c9e0a17ba5051c0c07f8df9
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-06-16 17:14:11 +0900

    [Gold IV] Title: 리모컨, Time: 468 ms, Memory: 282988 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.java
+++ 백준/Gold/1107. 리모컨/리모컨.java
@@ -5,6 +5,7 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static boolean[] brokenKey = new boolean[10];
+    public static int upsideMove, downsideMove, N, M;
     public static int upsideCheck(int num) {
         while (true) {
             boolean canBePressed = true;
@@ -12,6 +13,7 @@ public class Main {
                 if (brokenKey[ch - '0']) canBePressed = false;
             if (canBePressed) return num;
             num++;
+            if (downsideMove != -1 && Math.abs(num - N) + String.valueOf(num).length() > downsideMove) return -1;
         }
     }
     public static int downsideCheck(int num) {
@@ -27,7 +29,9 @@ public class Main {
     }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()), M = Integer.parseInt(br.readLine()), now = 100, res = 0;
+        N = Integer.parseInt(br.readLine());
+        M = Integer.parseInt(br.readLine());
+        int now = 100, res = 0;
         if (M != 0) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             for (int i = 0; i < M; i++) brokenKey[Integer.parseInt(st.nextToken())] = true;
@@ -46,8 +50,9 @@ public class Main {
         }
         else if (M == 10) System.out.println(Math.abs(100 - N)); // 숫자 키가 그냥 다 망가진 경우
         else {
-            int upsideMove = (upsideCheck(N) == -1) ? -1 : Math.abs(upsideCheck(N) - N) + String.valueOf(upsideCheck(N)).length();
-            int downsideMove = (downsideCheck(N) == -1) ? -1 : Math.abs(downsideCheck(N) - N) + String.valueOf(downsideCheck(N)).length();
+            downsideMove = (downsideCheck(N) == -1) ? -1 : Math.abs(downsideCheck(N) - N) + String.valueOf(downsideCheck(N)).length(); // 아래쪽 경우가 더 횟수 적음
+            upsideMove = (upsideCheck(N) == -1) ? -1 : Math.abs(upsideCheck(N) - N) + String.valueOf(upsideCheck(N)).length();
+
             int defMove = Math.abs(100 - N);
             if (upsideMove == -1 && downsideMove == -1) System.out.println(defMove);
             else if (upsideMove == -1) System.out.println(Math.min(downsideMove, defMove));

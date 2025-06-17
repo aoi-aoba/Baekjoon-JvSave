4f04a3e5b59083f2d8e6cc8663e7f7fb595b61cb
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-06-17 13:51:33 +0900

    [Gold V] Title: 링크와 스타트, Time: 140 ms, Memory: 12600 KB -BaekjoonHub

--- 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
+++ 백준/Gold/15661. 링크와 스타트/링크와 스타트.java
@@ -4,44 +4,48 @@ import java.io.IOException;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int N, result = Integer.MAX_VALUE;
+    public static int N, total = 0, result = Integer.MAX_VALUE;
     public static char[] teamTable;
+    public static int[] synergySum;
     public static int[][] synergyTable;
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
         N = Integer.parseInt(br.readLine());
         inputConverter();
-        tracker(0, 0);
+        tracker(0);
         System.out.println(result);
     }
 
     public static void inputConverter() throws IOException {
         synergyTable = new int[N][N];
         teamTable = new char[N];
+        synergySum = new int[N];
         for (int i = 0; i < N; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < N; j++)
+            for (int j = 0; j < N; j++) {
                 synergyTable[i][j] = Integer.parseInt(st.nextToken());
+                total += synergyTable[i][j];
+            }
+        }
+        for (int i = 0; i < N; i++) {
+            int sum = 0;
+            for (int j = 0; j < N; j++) sum += synergyTable[i][j] + synergyTable[j][i];
+            synergySum[i] = sum;
         }
     }
 
-    public static void tracker(int selectedNum, int startTeamNum) {
-        if (selectedNum > N / 2 && startTeamNum < 2) return;
+    public static void tracker(int selectedNum) {
         if (selectedNum == N) {
-            int startSum = 0, linkSum = 0;
+            int sum = 0;
             for (int i = 0; i < N; i++)
-                for (int j = i; j < N; j++)
-                    if (teamTable[i] == teamTable[j]) {
-                        if (teamTable[i] == 's') startSum += synergyTable[i][j] + synergyTable[j][i];
-                        else linkSum += synergyTable[i][j] + synergyTable[j][i];
-                    }
-            result = Math.min(result, Math.abs(startSum - linkSum));
+                if (teamTable[i] == 's') sum += synergySum[i];
+            result = Math.min(result, Math.abs(total - sum));
             return;
         }
         teamTable[selectedNum] = 's';
-        tracker(selectedNum + 1, startTeamNum + 1);
+        tracker(selectedNum + 1);
         teamTable[selectedNum] = 'l';
-        tracker(selectedNum + 1, startTeamNum);
+        tracker(selectedNum + 1);
     }
 }

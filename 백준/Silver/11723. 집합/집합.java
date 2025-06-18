9a6f35ceaefb8f0756d644343ec45334398061c9
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-06-18 22:32:20 +0900

    [Silver V] Title: 집합, Time: 1020 ms, Memory: 324928 KB -BaekjoonHub

--- 백준/Silver/11723. 집합/집합.java
+++ 백준/Silver/11723. 집합/집합.java
@@ -1,37 +1,45 @@
-import java.util.*;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringBuilder sb = new StringBuilder();
-
-        boolean[] arr = new boolean[20];
-        Arrays.fill(arr, false);
-
-        long M = Long.parseLong(br.readLine());
-        while(M > 0) {
-            String opLine = br.readLine();
-            if(opLine.contains("all")) {
-                Arrays.fill(arr, true);
-            } else if(opLine.contains("empty")) {
-                Arrays.fill(arr, false);
-            } else {
-                StringTokenizer st = new StringTokenizer(opLine);
-                String op = st.nextToken();
-                int i = Integer.parseInt(st.nextToken()) - 1;
-
-                if(op.equals("add")) arr[i] = true;
-                else if(op.equals("remove")) arr[i] = false;
-                else if(op.equals("toggle")) arr[i] = !arr[i];
-                else if(op.equals("check")) sb.append(arr[i] ? 1 : 0).append("\n");
+        StringBuilder result = new StringBuilder();
+        int arrayForBit = 0, M = Integer.parseInt(br.readLine());
+        while (M-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+            int temp;
+            switch (op) {
+                case "add" :
+                    temp = Integer.parseInt(st.nextToken()) - 1;
+                    arrayForBit = arrayForBit | (1 << temp);
+                    // 1 << temp는 temp번 비트만 1, 그 외는 0으로 만들기 때문에 |로 or 연산을 통해 반영해주면 됨
+                    break;
+                case "remove" :
+                    temp = Integer.parseInt(st.nextToken()) - 1;
+                    arrayForBit = arrayForBit & ~(1 << temp);
+                    // 1 << temp로 temp번 비트만 1로 만든 상태를 ~로 뒤집으면 지워야 하는 비트만 0이 되어 &로 변경 가능
+                    break;
+                case "check" :
+                    temp = Integer.parseInt(st.nextToken()) - 1;
+                    result.append((arrayForBit & (1 << temp)) == (1 << temp) ? 1 : 0).append("\n");
+                    break;
+                case "toggle" :
+                    temp = Integer.parseInt(st.nextToken()) - 1;
+                    arrayForBit = arrayForBit ^ (1 << temp);
+                    // 1 << temp는 temp번 비트만 1, 그 외는 0으로 만들기 때문에 |로 xor 연산을 통해 조건부 반영해주면 됨
+                    break;
+                case "all" :
+                    arrayForBit = (1 << 20) - 1;
+                    break;
+                case "empty" :
+                    arrayForBit = 0;
+                    break;
             }
-
-            M--;
         }
-
-        System.out.println(sb.toString());
-        br.close();
+        System.out.print(result);
     }
 }

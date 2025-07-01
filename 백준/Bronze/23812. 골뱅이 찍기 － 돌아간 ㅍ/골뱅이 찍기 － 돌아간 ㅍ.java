03dd9f96f75cf54bcffc226c49bd14025a8e8c94
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-07-01 19:31:43 +0900

    [Bronze III] Title: 골뱅이 찍기 - 돌아간 ㅍ, Time: 176 ms, Memory: 16996 KB -BaekjoonHub

---
+++ 백준/Bronze/23812. 골뱅이 찍기 － 돌아간 ㅍ/골뱅이 찍기 － 돌아간 ㅍ.java
@@ -0,0 +1,14 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) System.out.println("@".repeat(N) + " ".repeat(3*N) + "@".repeat(N));
+        for (int i = 0; i < N; i++) System.out.println("@".repeat(N*5));
+        for (int i = 0; i < N; i++) System.out.println("@".repeat(N) + " ".repeat(3*N) + "@".repeat(N));
+        for (int i = 0; i < N; i++) System.out.println("@".repeat(N*5));
+        for (int i = 0; i < N; i++) System.out.println("@".repeat(N) + " ".repeat(3*N) + "@".repeat(N));
+        br.close();
+    }
+}

d451f182fce9e6eddc058e0dd2f2cd80002d6973
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-07-16 23:15:31 +0900

    [Silver III] Title: 도미노 게임, Time: 424 ms, Memory: 143308 KB -BaekjoonHub

---
+++ 백준/Silver/34053. 도미노 게임/도미노 게임.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()), m = Integer.parseInt(st.nextToken());
+        long min = Integer.MAX_VALUE, sum = 0;
+
+        for (int i = 0; i < n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < m; j++) {
+                int temp = Integer.parseInt(st.nextToken());
+                sum += temp;
+                min = Math.min(temp, min);
+            }
+        }
+
+        bw.write(String.valueOf(sum - min));
+        bw.flush();
+    }
+}

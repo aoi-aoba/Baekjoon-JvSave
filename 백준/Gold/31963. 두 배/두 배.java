0e897d9903250852cfc98379b36a786d2211e89f
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-07-17 09:16:33 +0900

    [Gold III] Title: 두 배, Time: 296 ms, Memory: 42384 KB, Score: 39 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.java
+++ 백준/Gold/31963. 두 배/두 배.java
@@ -6,17 +6,17 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine()), result = 0;
-        int[] arr = new int[T];
+        int T = Integer.parseInt(br.readLine());
+        long result = 0;
+        int arr[] = new int[T], cnts[] = new int[T];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < T; i++) arr[i] = Integer.parseInt(st.nextToken());
 
         for (int i = 1; i < T; i++) {
-            if (arr[i] >= arr[i - 1]) continue;
-            else while (arr[i] < arr[i - 1]) {
-                arr[i] *= 2;
-                result++;
-            }
+            double log = Math.log((double) arr[i-1] / arr[i]) / Math.log(2);
+            int temp = ((int) log < log) ? (int)log + 1 : (int)log;
+            cnts[i] = temp + cnts[i - 1];
+            result += cnts[i];
         }
 
         bw.write(String.valueOf(result));

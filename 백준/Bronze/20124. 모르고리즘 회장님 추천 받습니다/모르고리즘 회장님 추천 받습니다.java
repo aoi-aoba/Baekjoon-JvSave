1bb33cf3b97cacfd7b21c2d5e06ddc00c3bcdb79
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-10 15:22:49 +0900

    [Bronze II] Title: 모르고리즘 회장님 추천 받습니다, Time: 260 ms, Memory: 39552 KB -BaekjoonHub

--- 백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.java
+++ 백준/Bronze/20124. 모르고리즘 회장님 추천 받습니다/모르고리즘 회장님 추천 받습니다.java
@@ -6,24 +6,21 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int T = Integer.parseInt(br.readLine());
-        HashMap<String, Integer> hashMap = new HashMap<>();
+        int T = Integer.parseInt(br.readLine()), maxNum = 0;
+        String name = "";
         for (int i = 0; i < T; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            hashMap.put(st.nextToken(), Integer.parseInt(st.nextToken()));
+            String temp = st.nextToken();
+            int tempNum = Integer.parseInt(st.nextToken());
+            if (tempNum > maxNum) {
+                maxNum = tempNum;
+                name = temp;
+            } else if (tempNum == maxNum) {
+                name = name.compareTo(temp) < 0 ? name : temp;
+            }
         }
 
-        List<String> nameList = new ArrayList<>(hashMap.keySet());
-        Collections.sort(nameList, new Comparator<String>() {
-            @Override
-            public int compare(String name1, String name2) {
-                if (hashMap.get(name1).equals(hashMap.get(name2)))
-                    return name1.compareTo(name2);
-                else return Integer.compare(hashMap.get(name2), hashMap.get(name1));
-            }
-        });
-
-        bw.write(String.valueOf(nameList.get(0)));
+        bw.write(String.valueOf(name));
         bw.flush();
     }
 }

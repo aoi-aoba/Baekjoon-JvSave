de45aca7ac42ac87493da70b64a4ed57159cd579
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-12 15:52:04 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 928 ms, Memory: 96276 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static List<List<Integer>> graph = new ArrayList<>();
+    public static boolean[] visited;
+    public static int[] visitNumArr;
+    public static int depth = 0;
+
+    public static void bfs(int startNode) {
+        visited[startNode] = true;
+        visitNumArr[startNode] = ++depth;
+
+        Queue<Integer> Q = new ArrayDeque<>();
+        Q.offer(startNode);
+        while (!Q.isEmpty()) {
+            int cur = Q.poll();
+            for (int node : graph.get(cur)) {
+                if (!visited[node]) {
+                    visited[node] = true;
+                    visitNumArr[node] = ++depth;
+                    Q.offer(node);
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int node = Integer.parseInt(st.nextToken());
+        int line = Integer.parseInt(st.nextToken());
+        int startNode = Integer.parseInt(st.nextToken());
+
+        visited = new boolean[node + 1];
+        visitNumArr = new int[node + 1];
+
+        for (int i = 0; i <= node; i++)
+            graph.add(new ArrayList<Integer>());
+
+        for (int i = 0; i < line; i++) {
+            st = new StringTokenizer(br.readLine());
+            int node1 = Integer.parseInt(st.nextToken()), node2 = Integer.parseInt(st.nextToken());
+            graph.get(node1).add(node2);
+            graph.get(node2).add(node1);
+        }
+
+        for (int i = 0; i <= node; i++)
+            Collections.sort(graph.get(i));
+
+        bfs(startNode);
+        for (int i = 1; i <= node; i++)
+            sb.append(visitNumArr[i]).append("\n");
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}

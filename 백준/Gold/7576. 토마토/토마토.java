c9d94869a4b84d7d3ba9043cb327a43e9bc5dfba
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-14 08:55:51 +0900

    [Gold V] Title: 토마토, Time: 476 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -5,26 +5,19 @@ public class Main {
     public static int M, N, checked = 0, result = 0;
     public static int[] dx = {-1, 1, 0, 0};
     public static int[] dy = {0, 0, -1, 1};
-    public static int[][] board, check;
-    public static List<int[]> startPoint;
+    public static int[][] board;
+    public static Queue<int[]> Q = new ArrayDeque<>();
 
     public static void bfs() {
-        Queue<int[]> Q = new ArrayDeque<>();
-        for (int i = 0; i < startPoint.size(); i++) {
-            Q.offer(new int[] {startPoint.get(i)[0], startPoint.get(i)[1], 0});
-            check[startPoint.get(i)[1]][startPoint.get(i)[0]] = -1;
-            checked++;
-        }
-
         while (!Q.isEmpty() && checked < M * N) {
             int[] cur = Q.poll();
             int x = cur[0], y = cur[1], d = cur[2];
 
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i], ny = y + dy[i];
-                if (nx >= 0 && nx < M && ny >= 0 && ny < N && board[ny][nx] == 0 && check[ny][nx] == 0) {
+                if (nx >= 0 && nx < M && ny >= 0 && ny < N && board[ny][nx] == 0) {
                     Q.offer(new int[] {nx, ny, d + 1});
-                    check[ny][nx] = d + 1;
+                    board[ny][nx] = d + 1;
                     result = Math.max(result, d + 1);
                     checked++;
                 }
@@ -40,18 +33,16 @@ public class Main {
         N = Integer.parseInt(st.nextToken());
 
         board = new int[N][M];
-        check = new int[N][M];
-        startPoint = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < M; j++) {
                 board[i][j] = Integer.parseInt(st.nextToken());
-                if (board[i][j] == -1) {
-                    check[i][j] = -1;
+                if (board[i][j] == -1) checked++;
+                else if (board[i][j] == 1) {
+                    Q.offer(new int[]{j, i, 0});
                     checked++;
                 }
-                else if (board[i][j] == 1) startPoint.add(new int[]{j, i});
             }
         }
 

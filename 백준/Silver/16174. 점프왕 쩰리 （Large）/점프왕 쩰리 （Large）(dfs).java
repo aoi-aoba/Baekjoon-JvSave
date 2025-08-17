4aaf15261fd63b0149d1fc8131eda42d07182d22
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-17 13:37:11 +0900

    Create 점프왕 쩰리 （Large）(dfs).java

---
+++ 백준/Silver/16174. 점프왕 쩰리 （Large）/점프왕 쩰리 （Large）(dfs).java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int[][] map;
+    public static boolean[][] visited;
+
+    public static void bfs() {
+        Queue<int[]> Q = new ArrayDeque<>();
+        Q.offer(new int[] {0, 0});
+        visited[0][0] = true;
+
+        while (!Q.isEmpty()) {
+            int[] cur = Q.poll();
+            int posX = cur[0], posY = cur[1], moveLen = map[posY][posX];
+
+            int[] dx = {moveLen, 0};
+            int[] dy = {0, moveLen};
+
+            for (int i = 0; i < 2; i++) {
+                int nx = posX + dx[i], ny = posY + dy[i];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[ny][nx]) {
+                    visited[ny][nx] = true;
+                    if (nx == N - 1 && ny == N - 1) return;
+                    Q.offer(new int[] {nx, ny});
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        bfs();
+        System.out.println(visited[N-1][N-1] ? "HaruHaru" : "Hing");
+    }
+}

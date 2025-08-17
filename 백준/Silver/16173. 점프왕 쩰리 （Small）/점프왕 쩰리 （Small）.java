fd1d1058900303b7bc6a694f6c64f34b41548cb4
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-08-17 13:39:14 +0900

    [Silver IV] Title: 점프왕 쩰리 (Small), Time: 64 ms, Memory: 11500 KB -BaekjoonHub

---
+++ 백준/Silver/16173. 점프왕 쩰리 （Small）/점프왕 쩰리 （Small）.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static int N;
+    public static int[][] map;
+    public static boolean[][] visited;
+
+    public static void dfs(int posX, int posY) {
+        int moveLen = map[posY][posX];
+        int[] dx = {moveLen, 0};
+        int[] dy = {0, moveLen};
+
+        for (int i = 0; i < 2; i++) {
+            int nx = posX + dx[i], ny = posY + dy[i];
+            if (nx >= 0 && nx < N && ny >= 0 && ny < N && !visited[ny][nx]) {
+                visited[ny][nx] = true;
+                if (nx == N - 1 && ny == N - 1) return;
+                dfs(nx, ny);
+            }
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
+        map = new int[N][N];
+        visited = new boolean[N][N];
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        dfs(0, 0);
+        System.out.println(visited[N-1][N-1] ? "HaruHaru" : "Hing");
+    }
+}

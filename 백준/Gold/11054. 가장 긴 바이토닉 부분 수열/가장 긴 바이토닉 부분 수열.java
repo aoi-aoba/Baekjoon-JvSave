d378fd240ec8f587cdf9645d258f7b3e391f7466
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-09-06 10:54:52 +0900

    [Gold IV] Title: 가장 긴 바이토닉 부분 수열, Time: 84 ms, Memory: 11980 KB -BaekjoonHub

--- 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
+++ 백준/Gold/11054. 가장 긴 바이토닉 부분 수열/가장 긴 바이토닉 부분 수열.java
@@ -2,36 +2,51 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
+    public static int n;
+    public static int[] reverse(int[] arr) {
+        int[] rev = new int[n];
+        for (int i = 0; i < n; i++)
+            rev[i] = arr[n - 1 - i];
+        return rev;
+    }
+
+    public static int[] lisLength(int[] arr) {
+        int[] dp = new int[n];
+        ArrayList<Integer> lis = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            int pos = Collections.binarySearch(lis, arr[i]);
+            if (pos < 0) pos = -(pos + 1);
+            // Collections.binarySearch는 그 값이 존재한다면 양수, 아니면 -(삽입위치)-1 반환
+            // 가령, {1, 2, 4, 5}에서 3을 돌리게 된다면 반환값은 -3으로 나오게 됨
+            if (pos == lis.size()) lis.add(arr[i]);
+            // pos가 맨 끝이라면 arr[i]가 최장 길이를 1 늘리는 꼬리가 되므로 append
+            else lis.set(pos, arr[i]);
+            // 맨 끝이 아니라면 그 자리에 원래 있던 값 대신 arr[i]를 저장
+            dp[i] = pos + 1;
+            // i는 0-based index이므로 1을 더해서 길이를 체크
+        }
+
+        return dp;
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        int N = Integer.parseInt(br.readLine()), result = 0;
-        int[] arr = new int[N], increaseDP = new int[N], decreaseDP = new int[N];
+        n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n], reversedArr = new int[n];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < n; i++)
             arr[i] = Integer.parseInt(st.nextToken());
 
-        for (int i = 0; i < N; i++) {
-            int max = 0;
-            for (int j = 0; j < i; j++)
-                if (arr[i] > arr[j])
-                    max = Math.max(max, increaseDP[j]);
-            increaseDP[i] = max + 1;
-        }
+        reversedArr = reverse(arr);
+        int[] increaseDP = lisLength(arr);
+        int[] decreaseDP = reverse(lisLength(reversedArr));
 
-        for (int i = N - 1; i >= 0 ; i--) {
-            int max = 0;
-            for (int j = N - 1; j >= i; j--)
-                if (arr[i] > arr[j])
-                    max = Math.max(max, decreaseDP[j]);
-            decreaseDP[i] = max + 1;
-        }
-
-        for (int i = 0; i < N; i++)
+        int result = 0;
+        for (int i = 0; i < n; i++)
             result = Math.max(result, increaseDP[i] + decreaseDP[i] - 1);
-
         System.out.println(result);
     }
 }

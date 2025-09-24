1fa17fa697531a3f5e1ed11f3e232b4fd72dfb49
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-09-24 21:40:56 +0900

    [Gold II] Title: 벡터 매칭, Time: 204 ms, Memory: 12724 KB -BaekjoonHub

---
+++ 백준/Gold/1007. 벡터 매칭/벡터 매칭.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static double res;
+    public static int N, xSum, ySum;
+    public static boolean[] visited;
+    public static ArrayList<int[]> points;
+    public static void tracker(int depth, int xPSum, int yPsum, int lastIdx) {
+        if (depth == N / 2) {
+            int v2xSum = xSum - xPSum, v2ySum = ySum - yPsum;
+            long dx = 2L * xPSum - xSum, dy = 2L * yPsum - ySum;
+            res = Math.min(res, Math.sqrt(dx * dx + dy * dy));
+            return;
+        }
+        for (int i = lastIdx; i < N; i++) {
+            if (!visited[i]) {
+                visited[i] = true;
+                int[] curP = points.get(i);
+                tracker(depth + 1, xPSum + curP[0], yPsum + curP[1], i);
+                visited[i] = false;
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        while (T-- > 0) {
+            N = Integer.parseInt(br.readLine());
+            visited = new boolean[N];
+            points = new ArrayList<>();
+            xSum = 0;
+            ySum = 0;
+            res = Double.MAX_VALUE;
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken()), y = Integer.parseInt(st.nextToken());
+                xSum += x;
+                ySum += y;
+                points.add(new int[] {x, y});
+            }
+
+            tracker(0, 0, 0, 0);
+            System.out.println(res);
+        }
+    }
+}

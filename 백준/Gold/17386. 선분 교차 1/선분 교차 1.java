88e62667f41accefe499ad3edcdaa69614a78490
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-09-25 16:32:29 +0900

    [Gold III] Title: 선분 교차 1, Time: 64 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Gold/17386. 선분 교차 1/선분 교차 1.java
@@ -0,0 +1,33 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static long[] x = new long[5];
+    public static long[] y = new long[5];
+
+    public static long crossProd(int a, int b, int c) {
+        return (x[b] - x[a]) * (y[c] - y[a]) - (y[b] - y[a]) * (x[c] - x[a]);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 1; i <= 2; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            x[2 * i - 1] = Long.parseLong(st.nextToken());
+            y[2 * i - 1] = Long.parseLong(st.nextToken());
+            x[2 * i] = Long.parseLong(st.nextToken());
+            y[2 * i] = Long.parseLong(st.nextToken());
+        }
+
+        long d1 = crossProd(1, 2, 3);
+        long d2 = crossProd(1, 2, 4);
+        long d3 = crossProd(3, 4, 1);
+        long d4 = crossProd(3, 4, 2);
+
+        boolean intersect = (d1 > 0 && d2 < 0 || d1 < 0 && d2 > 0)
+                && (d3 > 0 && d4 < 0 || d3 < 0 && d4 > 0);
+
+
+        System.out.println(intersect ? 1 : 0);
+    }
+}

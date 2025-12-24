cebb8263b55a16bf801b8a39dc1763e9d07fbc1b
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-12-24 16:31:57 +0900

    [Gold II] Title: 피보나치 수 3, Time: 84 ms, Memory: 23116 KB -BaekjoonHub

--- 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
+++ 백준/Gold/2749. 피보나치 수 3/피보나치 수 3.java
@@ -7,11 +7,11 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long n = Long.parseLong(br.readLine());
-        int lm = 1_500_000;
-        long[] dp = new long[lm + 1];
+        int idxToFind = (int) (n % 1_500_000);
+        long[] dp = new long[idxToFind + 1];
         dp[1] = 1;
-        for (int i = 2; i <= lm; i++)
+        for (int i = 2; i <= idxToFind; i++)
             dp[i] = (dp[i - 1] + dp[i - 2]) % 1_000_000;
-        System.out.println(dp[(int)(n % lm)]);
+        System.out.println(dp[idxToFind]);
     }
 }

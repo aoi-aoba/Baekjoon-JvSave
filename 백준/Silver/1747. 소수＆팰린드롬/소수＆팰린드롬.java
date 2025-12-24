982c0e01c26c452655b1a8734b7f0e960dd7e6c9
Park Seunghyeon <aoiaobaprod@gmail.com>
2025-12-24 21:41:56 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 256 ms, Memory: 58036 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,38 +1,37 @@
-//  소수&팰린드롬
-
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static boolean[] isPrime = new boolean[100_000_001];
-    public static boolean isPalindrome(int n) {
-        String ntos = String.valueOf(n);
-        StringBuilder sb = new StringBuilder();
-        for (int i = ntos.length() - 1; i >= 0; i--) {
-            sb.append(ntos.charAt(i));
-        }
-        return sb.toString().equals(ntos);
-    }
-    public static void sieve() {
-        Arrays.fill(isPrime, true);
-        isPrime[0] = isPrime[1] = false;
-        for (int i = 2; i <= 10000; i++) {
-            if (!isPrime[i]) continue;
-            for (int j = i * i; j <= 100_000_000; j += i) {
-                if (isPrime[j]) isPrime[j] = false;
-            }
-        }
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        sieve();
-        int N = Integer.parseInt(br.readLine());
-        int i = N;
-        while (true) {
-            if (isPrime[i] && isPalindrome(i)) {
-                System.out.println(i);
-                return;
-            } else i++;
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[10_000_001];
+
+        for (int i = 2; i < arr.length; i++) arr[i] = i;
+        for (int i = 2; i <= Math.sqrt(arr.length); i++) {
+            if (arr[i] == 0) continue;
+            for (int j = i * i; j < arr.length; j += i) arr[j] = 0;
         }
+
+        int i = n;
+        while (true) {
+            if (arr[i] != 0) {
+                if (isPalindrome(arr[i])) {
+                    System.out.println(arr[i]);
+                    return;
+                }
+            }
+            i++;
+        }
+    }
+
+    private static boolean isPalindrome(int result) {
+        char[] temp = String.valueOf(result).toCharArray();
+        int s = 0, e = temp.length - 1;
+        while (s < e) {
+            if (temp[s] != temp[e]) return false;
+            s++; e--;
+        }
+        return true;
     }
 }

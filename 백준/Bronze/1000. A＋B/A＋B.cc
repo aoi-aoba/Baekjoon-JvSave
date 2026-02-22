ee27ff43ea1c06c17dc118a561f82f1f76c04b99
Park Seunghyeon <aoiaobaprod@gmail.com>
2026-02-22 16:13:59 +0900

    [Bronze V] Title: A+B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.cc
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int main(void) {
+    fastio();
+    int a, b;
+    cin >> a >> b;
+    cout << a + b;
+    return 0;
+}

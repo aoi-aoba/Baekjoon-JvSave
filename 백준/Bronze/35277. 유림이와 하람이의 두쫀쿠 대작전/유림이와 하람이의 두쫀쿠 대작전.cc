775dc7ae1fa803c7e55887caf5f616f49c97b09d
Park Seunghyeon <aoiaobaprod@gmail.com>
2026-03-12 07:45:57 +0900

    [Bronze IV] Title: 유림이와 하람이의 두쫀쿠 대작전, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/35277. 유림이와 하람이의 두쫀쿠 대작전/유림이와 하람이의 두쫀쿠 대작전.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int main(void) {
+    fastio();
+    
+    int n;
+    cin >> n;
+    int doojjonku = 900 + 60 + 600 + 170 + 160 + 110;
+
+    cout << n / doojjonku;
+
+    return 0;
+}

93800bfc2faf05740d3d34ca17e0045f2e4531ed
Park Seunghyeon <aoiaobaprod@gmail.com>
2026-03-13 08:45:48 +0900

    [Bronze V] Title: 채플이 너무해, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/34945. 채플이 너무해/채플이 너무해.cc
@@ -0,0 +1,16 @@
+// 채플이 너무해
+#include <bits/stdc++.h>
+#define fastio() ios::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+using namespace std;
+
+int x;
+
+int main(void) {
+    fastio();
+    cin >> x;
+
+    if (x >= 6) cout << "Success!";
+    else cout << "Oh My God!";
+    
+    return 0;
+}
